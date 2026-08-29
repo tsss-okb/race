8d8e25ee5e6c24c3b093dc02cd02862210ddb6a3
tsss-okb <okbtssss@gmail.com>
2026-08-29 18:25:10 +0300

    Release 1.3 advanced video

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "ru.racelab.phone"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.2.0"
+        versionCode = 6
+        versionName = "1.3.0"
     }
 
     buildFeatures {
