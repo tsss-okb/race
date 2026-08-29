e86f3210e51713f96f6e9fbbd6b6972bef9d4d98
tsss-okb <okbtssss@gmail.com>
2026-08-29 18:19:33 +0300

    Release 1.2 lap analysis

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "ru.racelab.phone"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.1.0"
+        versionCode = 5
+        versionName = "1.2.0"
     }
 
     buildFeatures {
