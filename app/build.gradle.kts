8d175d544779afdfca09ae7b6b870f04a31dc8d0
tsss-okb <okbtssss@gmail.com>
2026-08-29 21:38:00 +0300

    Release 1.5 external BLE USB GNSS

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "ru.racelab.phone"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.4.0"
+        versionCode = 8
+        versionName = "1.5.0"
     }
 
     buildFeatures {
