e80b7e3c64d06493d01b53ee9996088519e98fd0
tsss-okb <okbtssss@gmail.com>
2026-08-30 01:18:40 +0300

    Release RaceLab 2.8 pit timing reliability

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "ru.racelab.phone"
         minSdk = 26
         targetSdk = 36
-        versionCode = 19
-        versionName = "2.7.0"
+        versionCode = 20
+        versionName = "2.8.0"
     }
 
     buildFeatures {
