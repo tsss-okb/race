050b856a3045d65b3e68b89e992aff558da14d6b
tsss-okb <okbtssss@gmail.com>
2026-08-30 00:23:00 +0300

    Release RaceLab 2.6 internet pit relay

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "ru.racelab.phone"
         minSdk = 26
         targetSdk = 36
-        versionCode = 17
-        versionName = "2.5.0"
+        versionCode = 18
+        versionName = "2.6.0"
     }
 
     buildFeatures {
