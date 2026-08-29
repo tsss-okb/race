c31da3eb53ef944a422d45ed6dc3643dabbb9275
tsss-okb <okbtssss@gmail.com>
2026-08-29 18:16:24 +0300

    Release 1.1 track library and ARM mode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "ru.racelab.phone"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.1.0"
     }
 
     buildFeatures {
