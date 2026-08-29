b57843da113f35c0777e4845bad02fb42d970cec
tsss-okb <okbtssss@gmail.com>
2026-08-29 21:33:00 +0300

    Release 1.4 extended OBD EV custom PID

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "ru.racelab.phone"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.3.0"
+        versionCode = 7
+        versionName = "1.4.0"
     }
 
     buildFeatures {
