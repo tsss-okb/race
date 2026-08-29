608a1c3062c9ad8ad1ccf2749fdf2fb174d99a34
tsss-okb <okbtssss@gmail.com>
2026-08-30 00:19:19 +0300

    Release RaceLab 2.5 pit lane screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "ru.racelab.phone"
         minSdk = 26
         targetSdk = 36
-        versionCode = 16
-        versionName = "2.4.0"
+        versionCode = 17
+        versionName = "2.5.0"
     }
 
     buildFeatures {
