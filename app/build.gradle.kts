a6d7a4e3a1432c8c14169de7b0f7cd80eba949c1
tsss-okb <okbtssss@gmail.com>
2026-08-30 02:25:38 +0300

    Release RaceLab 3.1 configurable PIT countdown

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "ru.racelab.phone"
         minSdk = 26
         targetSdk = 36
-        versionCode = 22
-        versionName = "3.0.0"
+        versionCode = 23
+        versionName = "3.1.0"
     }
 
     buildFeatures {
