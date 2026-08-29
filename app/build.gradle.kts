833d68d8a19e0836d104ea9246f8534fd2bf03c9
tsss-okb <okbtssss@gmail.com>
2026-08-29 23:34:24 +0300

    Release RaceLab 2.2 pit steering button

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "ru.racelab.phone"
         minSdk = 26
         targetSdk = 36
-        versionCode = 13
-        versionName = "2.1.2"
+        versionCode = 14
+        versionName = "2.2.0"
     }
 
     buildFeatures {
