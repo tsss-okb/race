953892a5587dc816095d3f8f6fdb5421087c484d
tsss-okb <okbtssss@gmail.com>
2026-09-02 23:50:49 +0300

    GT6 Target Lock v4.1 HS120 axis fix

--- gt6-target-lock/app/build.gradle.kts
+++ gt6-target-lock/app/build.gradle.kts
@@ -8,11 +8,11 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.tsss.gt6lock.v40"
+        applicationId = "com.tsss.gt6lock.v41"
         minSdk = 29
         targetSdk = 35
-        versionCode = 40
-        versionName = "4.0.0"
+        versionCode = 41
+        versionName = "4.1.0"
         externalNativeBuild {
             cmake { cppFlags += listOf("-O3", "-ffast-math", "-fno-exceptions", "-fno-rtti") }
         }
