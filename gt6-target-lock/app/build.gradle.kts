7934106384b271300e37f6b60116ace84b3d4e40
tsss-okb <okbtssss@gmail.com>
2026-09-03 01:10:34 +0300

    GT6 Target Lock v3.7 Adaptive Hold

--- gt6-target-lock/app/build.gradle.kts
+++ gt6-target-lock/app/build.gradle.kts
@@ -8,11 +8,11 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.tsss.gt6lock.v36"
+        applicationId = "com.tsss.gt6lock.v37"
         minSdk = 29
         targetSdk = 35
-        versionCode = 36
-        versionName = "3.6.0"
+        versionCode = 37
+        versionName = "3.7.0"
         externalNativeBuild {
             cmake { cppFlags += listOf("-O3", "-ffast-math", "-fno-exceptions", "-fno-rtti") }
         }
