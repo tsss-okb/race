a9fb8e7b4424adfe41a3f6df82277b5576e85684
tsss-okb <okbtssss@gmail.com>
2026-09-02 21:24:17 +0300

    GT6 Target Lock v1.0 unified camera axes

--- gt6-target-lock/app/build.gradle.kts
+++ gt6-target-lock/app/build.gradle.kts
@@ -8,11 +8,11 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.tsss.gt6lock.v09"
+        applicationId = "com.tsss.gt6lock.v10"
         minSdk = 29
         targetSdk = 35
-        versionCode = 9
-        versionName = "0.9.0"
+        versionCode = 10
+        versionName = "1.0.0"
         externalNativeBuild {
             cmake { cppFlags += listOf("-O3", "-ffast-math", "-fno-exceptions", "-fno-rtti") }
         }
