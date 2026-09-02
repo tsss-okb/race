8443a54df0197bd3c518957ff6fd125e21b22191
tsss-okb <okbtssss@gmail.com>
2026-09-02 20:19:14 +0300

    GT6 Target Lock v0.5

--- gt6-target-lock/app/build.gradle.kts
+++ gt6-target-lock/app/build.gradle.kts
@@ -8,11 +8,11 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.tsss.gt6lock.v04"
+        applicationId = "com.tsss.gt6lock.v05"
         minSdk = 29
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.4.0"
+        versionCode = 5
+        versionName = "0.5.0"
         externalNativeBuild {
             cmake { cppFlags += listOf("-O3", "-ffast-math", "-fno-exceptions", "-fno-rtti") }
         }
