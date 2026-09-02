acb9cd48007f3d8a68d08016b836a39ea1ba7488
tsss-okb <okbtssss@gmail.com>
2026-09-03 00:22:03 +0300

    GT6 Target Lock v3.3 120Hz display request

--- gt6-target-lock/app/build.gradle.kts
+++ gt6-target-lock/app/build.gradle.kts
@@ -8,11 +8,11 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.tsss.gt6lock.v32"
+        applicationId = "com.tsss.gt6lock.v33"
         minSdk = 29
         targetSdk = 35
-        versionCode = 32
-        versionName = "3.2.0"
+        versionCode = 33
+        versionName = "3.3.0"
         externalNativeBuild {
             cmake { cppFlags += listOf("-O3", "-ffast-math", "-fno-exceptions", "-fno-rtti") }
         }
